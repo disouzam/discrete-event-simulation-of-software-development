489da9b5fe22e1fac56f1da0fe019ac10dcd601b
Greg Wilson <gvwilson@third-bit.com>
2025-12-09 17:21:32 -0500

    log-normal distribution

---
+++ simple/plot_log_normal.py
@@ -0,0 +1,16 @@
+import numpy as np
+import plotly.express as px
+import polars as pl
+from scipy.stats import lognorm
+import sys
+
+mu = 0
+sigma = 1
+
+dist = lognorm(s=sigma, scale=np.exp(mu))
+x = np.linspace(dist.ppf(0.001), dist.ppf(0.99), 500)
+y = dist.pdf(x)
+
+df = pl.DataFrame({"x": x, "y": y})
+fig = px.line(df, x="x", y="y")
+fig.write_image(sys.argv[1])
