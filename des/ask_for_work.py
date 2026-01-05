0cfeaa00a37565189f6dd88c02ea857461edeb61
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-04 22:10:03 -0300

    Added more context to docstring of `ask_for_work.py`

--- des/ask_for_work.py
+++ des/ask_for_work.py
@@ -1,4 +1,8 @@
-"""Ask for work at regular intervals."""
+"""
+A developer ask for work at regular intervals.
+A Scrum Master answers one single time informing there is available and ready work.
+There is no interaction between them at this moment.
+"""
 
 from simpy import Environment
 
@@ -11,9 +15,11 @@ def coder(env):
         print(f"{env.now}: Is there any work?")
         yield env.timeout(T_WAIT)
 
+
 def scrum_master(env):
     yield env.timeout(14)
     print(f"{env.now}: There is a new task available for you, developer.")
+
 
 if __name__ == "__main__":
     env = Environment()
