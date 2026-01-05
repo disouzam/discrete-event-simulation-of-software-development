1c914ff1acf4bc26cef690225e29fcb562eb30c9
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-04 21:58:39 -0300

    Added a new process to represent Scrum Master making work available for developer

--- des/ask_for_work.py
+++ des/ask_for_work.py
@@ -11,8 +11,12 @@ def coder(env):
         print(f"{env.now}: Is there any work?")
         yield env.timeout(T_WAIT)
 
+def scrum_master(env):
+    yield env.timeout(14)
+    print(f"{env.now}: There is a new task available for you, developer.")
 
 if __name__ == "__main__":
     env = Environment()
     env.process(coder(env))
+    env.process(scrum_master(env))
     env.run(until=T_SIM)
