d5db72067d824903c2330cfd7c97207e08cdd8c9
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-07 20:48:34 -0300

    Tentative use of colors from colored package in marimo's output

--- des/simple_interaction_marimo_named_cell_with_colors.py
+++ des/simple_interaction_marimo_named_cell_with_colors.py
@@ -29,7 +29,7 @@ def imports():
 
 @app.cell
 def _(cd):
-    WHITE_ON_BLACK = f"{cd.fore_rgb(255,255,255)}{cd.back_rgb(0,0,0)}"
+    WHITE_ON_BLACK = f"{cd.fore_rgb(255, 255, 255)}{cd.back_rgb(0, 0, 0)}"
     return (WHITE_ON_BLACK,)
 
 
@@ -43,24 +43,35 @@ def sim_constants():
 
 
 @app.cell
-def job_class(T_JOB, count):
+def _(cd):
+    color_names = list(cd.library.Library.COLORS.keys())
+    color_codes = list(cd.library.Library.COLORS.values())
+    swapped_codes = list(zip(color_codes, color_names))
+
+    job_colors = {int(color_code): color_name for color_code, color_name in swapped_codes}
+    return (job_colors,)
+
+
+@app.cell
+def job_class(T_JOB, cd, count):
     class Job:
         _next_id = count()
 
-        def __init__(self):
+        def __init__(self, job_colors):
             self.id = next(Job._next_id)
             self.duration = T_JOB
+            self.job_colors = job_colors
 
         def __str__(self):
-            return f"job {self.id}"
+            return f"job {cd.fore(self.job_colors[self.id])}{cd.Back.black}{self.id}{cd.Style.reset}"
     return (Job,)
 
 
 @app.cell
 def manager_process(Job, T_CREATE, WHITE_ON_BLACK, cd):
-    def manager(env, queue):
+    def manager(env, queue, job_colors):
         while True:
-            job = Job()
+            job = Job(job_colors)
             print(f"At {WHITE_ON_BLACK} {env.now:>2} {cd.Style.reset}, manager creates {job}")
             yield queue.put(job)
             yield env.timeout(T_CREATE)
@@ -73,6 +84,7 @@ def _(WHITE_ON_BLACK, cd):
         while True:
             wait_starts = env.now
             job = yield queue.get()
+            print(queue)
 
             get_job_at = env.now
 
@@ -80,21 +92,25 @@ def _(WHITE_ON_BLACK, cd):
                 print(f"At {WHITE_ON_BLACK} {wait_starts:>2} {cd.Style.reset}, coder waits")
                 print(f"At {WHITE_ON_BLACK} {get_job_at:>2} {cd.Style.reset}, coder gets job {job}")
             else:
-                print(f"At {WHITE_ON_BLACK} {get_job_at:>2} {cd.Style.reset}, coder gets job {job} without waiting")
+                print(
+                    f"At {WHITE_ON_BLACK} {get_job_at:>2} {cd.Style.reset}, coder gets job {job} without waiting"
+                )
 
             yield env.timeout(job.duration)
 
             completed_job_at = env.now
-            print(f"At {WHITE_ON_BLACK} {completed_job_at:>2} {cd.Style.reset}, code completes job {job}")
+            print(
+                f"At {WHITE_ON_BLACK} {completed_job_at:>2} {cd.Style.reset}, code completes job {job}"
+            )
     return (coder,)
 
 
 @app.cell
-def entry_point(Environment, Store, T_SIM, coder, manager):
+def entry_point(Environment, Store, T_SIM, coder, job_colors, manager):
     def main():
         env = Environment()
         queue = Store(env)
-        env.process(manager(env, queue))
+        env.process(manager(env, queue, job_colors))
         env.process(coder(env, queue))
         env.run(until=T_SIM)
     return (main,)
