875b706114fd33e1f0ea8b79cf472eb55fc68349
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-08 07:52:48 -0300

    Implemented an algorithm to colorize job id with a fixed foreground and background colors in all output display
    - A better calculation must be implemented to guarantee enough contrast to see job id
    - Modified display of output
    - Modified coder, manager and job to accept parameters to configure simulation

--- des/simple_interaction_marimo_named_cell_with_colors.py
+++ des/simple_interaction_marimo_named_cell_with_colors.py
@@ -29,52 +29,37 @@ def imports():
 
 @app.cell
 def _(cd):
-    WHITE_ON_BLACK = f"{cd.fore_rgb(255, 255, 255)}{cd.back_rgb(0, 0, 0)}"
+    WHITE_ON_BLACK = f"{cd.fore_rgb(255,255,255)}{cd.back_rgb(0,0,0)}"
     return (WHITE_ON_BLACK,)
 
 
 @app.cell
-def sim_constants():
-    # Simulation constants
-    T_CREATE = 6
-    T_JOB = 8
-    T_SIM = 20
-    return T_CREATE, T_JOB, T_SIM
-
-
-@app.cell
-def _(cd):
-    color_names = list(cd.library.Library.COLORS.keys())
-    color_codes = list(cd.library.Library.COLORS.values())
-    swapped_codes = list(zip(color_codes, color_names))
-
-    job_colors = {int(color_code): color_name for color_code, color_name in swapped_codes}
-    return (job_colors,)
-
-
-@app.cell
-def job_class(T_JOB, cd, count):
+def job_class(count):
     class Job:
-        _next_id = count()
+        _next_id = count(start=0)
 
-        def __init__(self, job_colors):
+        def __init__(self, job_duration):
             self.id = next(Job._next_id)
-            self.duration = T_JOB
-            self.job_colors = job_colors
+            self.duration = job_duration
 
         def __str__(self):
-            return f"job {cd.fore(self.job_colors[self.id])}{cd.Back.black}{self.id}{cd.Style.reset}"
+            # return f"job {fg_colors[self.id]}{bg_colors[self.id + 15]} {cd.Style.bold}{self.id} {cd.Style.reset}"
+            return f"job {self.id}"
+
+        def __repr__(self):
+            return self.__str__()
     return (Job,)
 
 
 @app.cell
-def manager_process(Job, T_CREATE, WHITE_ON_BLACK, cd):
-    def manager(env, queue, job_colors):
+def manager_process(Job, WHITE_ON_BLACK, cd):
+    def manager(env, queue, time_between_new_tasks, job_duration):
         while True:
-            job = Job(job_colors)
-            print(f"At {WHITE_ON_BLACK} {env.now:>2} {cd.Style.reset}, manager creates {job}")
-            yield queue.put(job)
-            yield env.timeout(T_CREATE)
+            job = Job(job_duration)
+            print(f"\nQueue items before manager creates one more job: {queue.items}")
+            print(f"At {WHITE_ON_BLACK} {env.now:>2}  {cd.Style.reset}, manager creates {job}")
+            yield queue.put(job)        
+            yield env.timeout(time_between_new_tasks)
     return (manager,)
 
 
@@ -83,42 +68,49 @@ def _(WHITE_ON_BLACK, cd):
     def coder(env, queue):
         while True:
             wait_starts = env.now
+            print(f"\nQueue items before coder takes one job: {queue.items}")
             job = yield queue.get()
-            print(queue)
 
             get_job_at = env.now
 
             if get_job_at - wait_starts > 0:
-                print(f"At {WHITE_ON_BLACK} {wait_starts:>2} {cd.Style.reset}, coder waits")
-                print(f"At {WHITE_ON_BLACK} {get_job_at:>2} {cd.Style.reset}, coder gets job {job}")
+                print(f"At {WHITE_ON_BLACK} {wait_starts:>2}  {cd.Style.reset}, coder waits")
+                print(f"At {WHITE_ON_BLACK} {get_job_at:>2}  {cd.Style.reset}, coder gets {job}")
             else:
                 print(
-                    f"At {WHITE_ON_BLACK} {get_job_at:>2} {cd.Style.reset}, coder gets job {job} without waiting"
+                    f"At {WHITE_ON_BLACK} {get_job_at:>2}  {cd.Style.reset}, coder gets {job} without waiting"
                 )
 
             yield env.timeout(job.duration)
 
             completed_job_at = env.now
-            print(
-                f"At {WHITE_ON_BLACK} {completed_job_at:>2} {cd.Style.reset}, code completes job {job}"
-            )
+            print(f"At {WHITE_ON_BLACK} {completed_job_at:>2}  {cd.Style.reset}, code completes {job}")
     return (coder,)
 
 
 @app.cell
-def entry_point(Environment, Store, T_SIM, coder, job_colors, manager):
-    def main():
+def entry_point(Environment, Store, T_SIM, coder, manager):
+    def run_simulation(time_between_new_tasks, job_duration):
         env = Environment()
         queue = Store(env)
-        env.process(manager(env, queue, job_colors))
+        env.process(manager(env, queue, time_between_new_tasks, job_duration))
         env.process(coder(env, queue))
         env.run(until=T_SIM)
-    return (main,)
+    return (run_simulation,)
 
 
 @app.cell
-def _(main):
-    main()
+def _():
+    # Simulation constants
+    T_CREATE = 6
+    T_JOB = 8
+    T_SIM = 20
+    return T_CREATE, T_JOB, T_SIM
+
+
+@app.cell
+def _(T_CREATE, T_JOB, run_simulation):
+    run_simulation(T_CREATE, T_JOB)
     return
 
 
