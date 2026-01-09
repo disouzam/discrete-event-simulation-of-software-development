6422871ec2ce6fade2cd4e9ce005124f3166959b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-08 22:07:07 -0300

    Implemented functions that get the best background color and adjusted job creation to print job id using random colors
    Sources:
    - https://codepen.io/AndrewKnife/pen/XWBggQq
    - https://stackoverflow.com/questions/946544/good-text-foreground-color-for-a-given-background-color/75110271#75110271

--- des/simple_interaction_marimo_named_cell_with_colors.py
+++ des/simple_interaction_marimo_named_cell_with_colors.py
@@ -29,23 +29,32 @@ def imports():
 
 @app.cell
 def _(cd):
-    WHITE_ON_BLACK = f"{cd.fore_rgb(255,255,255)}{cd.back_rgb(0,0,0)}"
-    WHITE_ON_RED = f"{cd.fore_rgb(255,255,255)}{cd.back_rgb(255,0,0)}"
+    WHITE_ON_BLACK = f"{cd.fore_rgb(255, 255, 255)}{cd.back_rgb(0, 0, 0)}"
+    WHITE_ON_RED = f"{cd.fore_rgb(255, 255, 255)}{cd.back_rgb(255, 0, 0)}"
     return WHITE_ON_BLACK, WHITE_ON_RED
 
 
 @app.cell
-def job_class(count):
+def job_class(cd, count, getContrastColor):
+    import numpy as np
+
+    rand = np.random.default_rng(seed=2026)
+
+
     class Job:
         _next_id = count(start=0)
 
         def __init__(self, job_duration):
             self.id = next(Job._next_id)
             self.duration = job_duration
+            red = rand.uniform(0, 255)
+            green = rand.uniform(0, 255)
+            blue = rand.uniform(0, 255)
+            self.fore = cd.fore_rgb(red, green, blue)
+            self.back = getContrastColor(red, green, blue)
 
         def __str__(self):
-            # return f"job {fg_colors[self.id]}{bg_colors[self.id + 15]} {cd.Style.bold}{self.id} {cd.Style.reset}"
-            return f"job {self.id}"
+            return f"job {self.fore}{self.back} {cd.Style.bold}{self.id} {cd.Style.reset}"
 
         def __repr__(self):
             return self.__str__()
@@ -59,7 +68,7 @@ def manager_process(Job, WHITE_ON_BLACK, cd):
             job = Job(job_duration)
             print(f"\nQueue items before manager creates one more job: {queue.items}")
             print(f"At {WHITE_ON_BLACK} {env.now:>2}  {cd.Style.reset}, manager creates {job}")
-            yield queue.put(job)        
+            yield queue.put(job)
             yield env.timeout(time_between_new_tasks)
     return (manager,)
 
@@ -75,7 +84,9 @@ def _(WHITE_ON_BLACK, WHITE_ON_RED, cd):
             get_job_at = env.now
 
             if get_job_at - wait_starts > 0:
-                print(f"{WHITE_ON_RED}At {WHITE_ON_BLACK} {wait_starts:>2}  {WHITE_ON_RED}, coder waits{cd.Style.reset}")
+                print(
+                    f"{WHITE_ON_RED}At {WHITE_ON_BLACK} {wait_starts:>2}  {WHITE_ON_RED}, coder waits{cd.Style.reset}"
+                )
                 print(f"At {WHITE_ON_BLACK} {get_job_at:>2}  {cd.Style.reset}, coder gets {job}")
             else:
                 print(
@@ -128,6 +139,39 @@ def _(run_simulation):
     _T_SIM = 20
     run_simulation(_T_CREATE, _T_JOB, _T_SIM)
     return
+
+
+@app.cell
+def _(cd):
+    # Sources:
+    # https://codepen.io/AndrewKnife/pen/XWBggQq
+    # https://stackoverflow.com/a/75110271/5781015
+
+    import math
+
+
+    def calculateLight(colorItem: int):
+        c = colorItem / 255
+
+        if c < 0.03928:
+            c /= 12.92
+        else:
+            c = math.pow((c + 0.055) / 1.055, 2.4)
+
+        return c
+
+
+    def calculateLuminosity(r: int, g: int, b: int):
+        return 0.2126 * calculateLight(r) + 0.7152 * calculateLight(g) + 0.0722 * calculateLight(b)
+
+
+    def getContrastColor(r: int, g: int, b: int):
+        LUMINOSITY_LIMIT = 0.579  # This is the Contrast Threshold, the higher it is, the more likely text will be white
+        if calculateLuminosity(r, g, b) > LUMINOSITY_LIMIT:
+            return cd.back_rgb(0, 0, 0)
+
+        return cd.back_rgb(255, 255, 255)
+    return (getContrastColor,)
 
 
 if __name__ == "__main__":
