08d95a121a312865aff5acfc212f7faeb70e779b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2026-01-24 20:50:48 -0300

    Moved logic of ANSI escape characters sanitization to script `strip.sh`

--- des/strip.sh
+++ des/strip.sh
@@ -1,1 +1,9 @@
-cat simple_interaction_marimo_alternative_output.txt | sed 's/[^[:print:]]//g' | sed 's/\[[0-9]\+;[0-9]\+;[0-9]\+;[0-9]\+;[0-9]\+m//g' | sed 's/\[[0-9]\+;[0-9]\+;[0-9]\+m//g' | sed 's/\[0m//g' | sed 's/\[1m//g' | sed 's/  / /g'
+#!/bin/bash
+# Strip ANSI escape characters used to colorize output
+cat $1 | sed 's/[^[:print:]]//g' | sed 's/\[[0-9]\+;[0-9]\+;[0-9]\+;[0-9]\+;[0-9]\+m//g' | sed 's/\[[0-9]\+;[0-9]\+;[0-9]\+m//g' | sed 's/\[0m//g' | sed 's/\[1m//g' | sed 's/  / /g' > $2
+
+# Remove original file
+rm -rf $1
+
+# Rename sanitized file
+mv $2 $1
